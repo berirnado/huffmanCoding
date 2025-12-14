324740476423994fa0f298e7ee17df9669715ed2
Bernardo Robaina <bernardo.robaina@serel.com.br>
2025-12-14 20:50:54 -0300

    Funcao para gerar o array de frequencia dos bytes

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void gerarArrayDeFrequencia(FILE* fptr, int* freq){
+    int c;
+
+    while((c = fgetc(fptr)) != EOF){ // cada linha do arquivo
+        freq[(unsigned char)c]++;
+    }
+}
 
 void main(){
-    printf("Hello world!");
+    int freq[256] = {0}; // array utilizado pra armazenar a frequencia de cada byte
+
+    FILE* fptr = fopen("palavras.txt", "r");
+
+    if(fptr == NULL){
+        printf("Erro ao abrir arquivo");
+        exit(1);
+    }
+
+    gerarArrayDeFrequencia(fptr, freq);
+    fclose(fptr);
+
+    for(int i = 0; i < 256; ++i){
+        printf("%d\n", freq[i]);
+    }
 }
